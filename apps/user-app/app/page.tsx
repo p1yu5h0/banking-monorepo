d5a621c2368d655f9b0027d93bf8490afd52655b
p1yu5h0 <piyush200205@gmail.com>
2024-03-28 09:05:38 +0530

    added db

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+import {PrismaClient} from "@repo/db/client"
+
+const client = new PrismaClient();
+
+
+export default function Page(): JSX.Element {
+  return (
+    <div className="text-2xl">
+      Hi there
+    </div>
+  );
+}
